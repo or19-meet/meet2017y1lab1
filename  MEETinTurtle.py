273053a3b7d6bd5f4bd6393ccc7f54f9b0813c45
or19-meet <or19@meet.mit.edu>
2017-07-23 22:03:52 +0300

    Stopping here

---
+++  MEETinTurtle.py
@@ -0,0 +1,26 @@
+import turtle
+
+#comments are amazing, please comment on my work
+
+turtle.penup()
+turtle.goto(-200, -100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(-50,-100+200)
+turtle.goto(-50+100,100)
+turtle.penup()
+turtle.goto(-50,-100+100)
+turtle.pendown
+
+turtle.mainloop()
+turtle.mainloop()
+
+
+
